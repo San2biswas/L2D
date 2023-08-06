1d5c73a297096464790a784fe8a43dd55c5dc560
San2 <santu.santu.biswas@gmail.com>
2023-08-06 20:30:31 +0530

    Update L2D
    Update :
    
    - added a base cursor
    - added hover animation with elements
    - on click a text will show which one you clicked

---
+++ L2D_RPS/Game/elements.lua
@@ -0,0 +1,36 @@
+ -- rock properties
+ oRock = {}
+ oRock.sprite = love.graphics.newImage("Assets/Sprites/UI/Rock.png");
+ oRock.x = 25;
+ oRock.y = 10;
+ oRock.sX = 1;
+ oRock.sY = 1;
+ oRock.rot = 0;
+ oRock.w = oRock.sprite:getWidth();
+ oRock.h = oRock.sprite:getHeight();
+ 
+ 
+ -- paper properties
+ oPaper = {}
+ oPaper.sprite = love.graphics.newImage("Assets/Sprites/UI/Paper.png");
+ oPaper.x = 225;
+ oPaper.y = 10;
+ oPaper.sX = 1;
+ oPaper.sY = 1;
+ oPaper.rot = 0;
+ oPaper.w = oPaper.sprite:getWidth();
+ oPaper.h = oPaper.sprite:getHeight();
+ 
+ 
+ -- scissors properties
+ oScissors = {}
+ oScissors.sprite = love.graphics.newImage("Assets/Sprites/UI/Scissors.png");
+ oScissors.x = 425;
+ oScissors.y = 10;
+ oScissors.sX = 1;
+ oScissors.sY = 1;
+ oScissors.rot = 0;
+ oScissors.w = oScissors.sprite:getWidth();
+ oScissors.h = oScissors.sprite:getHeight();
+ 
+ 
